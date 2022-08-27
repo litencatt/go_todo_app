2e1e66b1fcf26d4bed4604041bc9af8cdf81aaec
litencatt <ncl0709@gmail.com>
2022-08-27 11:23:50 +0900

    CI時のDBホスト修正

--- testutil/db.go
+++ testutil/db.go
@@ -14,13 +14,15 @@ func OpenDBForTest(t *testing.T) *sqlx.DB {
 	t.Helper()
 
 	// テスト実行はdocker内で行うため
+	host := "todo-db"
 	port := 3306
 	if _, defined := os.LookupEnv("CI"); defined {
+		host = "127.0.0.1"
 		port = 3306
 	}
 	db, err := sql.Open(
 		"mysql",
-		fmt.Sprintf("todo:todo@tcp(todo-db:%d)/todo?parseTime=true", port),
+		fmt.Sprintf("todo:todo@tcp(%s:%d)/todo?parseTime=true", host, port),
 	)
 	if err != nil {
 		t.Fatal(err)
