84f274c66de082a2f6f8e4ae6f12e6df7f4df9d6
litencatt <ncl0709@gmail.com>
2022-09-05 17:56:32 +0900

    ミドルウェア実装

---
+++ handler/middleware.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/litencatt/go_todo_app/auth"
+)
+
+// contextにユーザー情報を埋め込む
+func AuthMiddlewar(j *auth.JWTer) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			req, err := j.FillContext(r)
+			// アクセストークンが見つからなければ401を返す
+			if err != nil {
+				RespondJSON(r.Context(), w, ErrResponse{
+					Message: "not find auth info",
+					Details: []string{err.Error()},
+				}, http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
+func AdminMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !auth.IsAdmin(r.Context()) {
+			RespondJSON(r.Context(), w, ErrResponse{
+				Message: "not admin",
+			}, http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
